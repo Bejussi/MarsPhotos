052ac67e51d558ca6075af3453056f53d94e553b
Alexandra <pindooqy@gmail.com>
2022-07-12 12:57:01 +0300

    Display a grid of images with a RecyclerView

--- app/src/main/java/com/bejussi/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/bejussi/marsphotos/BindingAdapters.kt
@@ -1,9 +1,14 @@
 package com.bejussi.marsphotos
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.bejussi.marsphotos.network.MarsPhoto
+import com.bejussi.marsphotos.overview.MarsApiStatus
+import com.bejussi.marsphotos.overview.PhotoGridAdapter
 
 class BindingAdapters {
 }
@@ -15,6 +20,30 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         imgView.load(imgUri) {
             placeholder(R.drawable.loading_animation)
             error(R.drawable.ic_broken_image)
+        }
+    }
+}
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView,
+                     data: List<MarsPhoto>?) {
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView, status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
         }
     }
 }
